56a6287c7249c29c05c08a6d5b33d0527c3ac3c3
congthang <congthang89@gmail.com>
2021-08-21 19:40:50 +0700

    update changing value

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl config use-context your-kubernetes-context-authenticated <<update this>>
+kubectl config use-context your-kubernetes-context-authenticated # <<update this>>
 kubectl apply -f kubernetes/bbb-config.yaml
 kubectl apply -f kubernetes/mongo-configMap.yaml
 kubectl apply -f kubernetes/bbb-statefullset.yaml
